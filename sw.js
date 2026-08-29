f589dc8dca9eef0193bfcc98f279cc42eb8c3e82
mohamedelsayedwor-wq <mohamedelsayedwor@gmail.com>
2026-08-29 20:05:44 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,103 @@
+const CACHE_NAME = "studydesk-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json"
+];
+
+
+/* =========================
+   INSTALL
+========================= */
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+                .then(cache => {
+
+                    return cache.addAll(
+                        FILES_TO_CACHE
+                    );
+
+                })
+
+        );
+
+        self.skipWaiting();
+
+    }
+);
+
+
+/* =========================
+   ACTIVATE
+========================= */
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+                .then(names => {
+
+                    return Promise.all(
+
+                        names
+                            .filter(
+                                name =>
+                                    name !== CACHE_NAME
+                            )
+                            .map(
+                                name =>
+                                    caches.delete(name)
+                            )
+
+                    );
+
+                })
+
+        );
+
+        self.clients.claim();
+
+    }
+);
+
+
+/* =========================
+   FETCH
+========================= */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(event.request)
+                .then(cachedResponse => {
+
+                    if (cachedResponse) {
+
+                        return cachedResponse;
+
+                    }
+
+
+                    return fetch(event.request);
+
+                })
+
+        );
+
+    }
+);
